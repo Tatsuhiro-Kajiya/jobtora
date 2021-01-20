a014bc7f157f0d4edf94723770ee3b9e93a8d0bb
Tatsuhiro-Kajiya <75005529+Tatsuhiro-Kajiya@users.noreply.github.com>
2021-01-20 20:26:16 +0900

    新規登録、ログイン、ログアウト機能実装

--- db/migrate/20210119032801_devise_create_users.rb
+++ db/migrate/20210119032801_devise_create_users.rb
@@ -5,9 +5,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :nickname,           null: false
-      t.string :name,               null: false
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :age,                null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      
 
       ## Recoverable
       t.string   :reset_password_token
